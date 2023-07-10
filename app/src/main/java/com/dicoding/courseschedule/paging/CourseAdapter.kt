5325f5685fcd8e2e60902d9999289c9ffecaaabb
Dwiko Indrawansyah <wesibrani@gmail.com>
2023-07-10 14:00:17 +0700

    CourseAdapter

--- app/src/main/java/com/dicoding/courseschedule/paging/CourseAdapter.kt
+++ app/src/main/java/com/dicoding/courseschedule/paging/CourseAdapter.kt
@@ -1,11 +1,13 @@
 package com.dicoding.courseschedule.paging
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import com.dicoding.courseschedule.data.Course
+import com.dicoding.courseschedule.databinding.ItemCourseBinding
 
-//TODO 6 : Implement Method for PagedListAdapter
+// Implement Method for PagedListAdapter
 class CourseAdapter(private val clickListener: (Course) -> Unit) :
     PagedListAdapter<Course, CourseViewHolder>(DIFF_CALLBACK) {
 
@@ -22,10 +24,17 @@ class CourseAdapter(private val clickListener: (Course) -> Unit) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
-        throw NotImplementedError()
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemCourseBinding.inflate(inflater, parent, false)
+        return CourseViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
-
+        val course = getItem(position)
+        if (course != null) {
+            holder.bind(course, clickListener)
+        } else {
+            holder.clear()
+        }
     }
 }
--- app/src/main/java/com/dicoding/courseschedule/paging/CourseViewHolder.kt
+++ app/src/main/java/com/dicoding/courseschedule/paging/CourseViewHolder.kt
@@ -4,14 +4,15 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.courseschedule.R
 import com.dicoding.courseschedule.data.Course
+import com.dicoding.courseschedule.databinding.ItemCourseBinding
 import com.dicoding.courseschedule.util.DayName.Companion.getByNumber
 
-class CourseViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class CourseViewHolder(private val binding: ItemCourseBinding): RecyclerView.ViewHolder(binding.root) {
 
     private lateinit var course: Course
     private val timeString = itemView.context.resources.getString(R.string.time_format)
 
-    //TODO 7 : Complete ViewHolder to show item
+    // Complete ViewHolder to show item
     fun bind(course: Course, clickListener: (Course) -> Unit) {
         this.course = course
 
@@ -19,12 +20,21 @@ class CourseViewHolder(view: View): RecyclerView.ViewHolder(view) {
             val dayName = getByNumber(day)
             val timeFormat = String.format(timeString, dayName, startTime, endTime)
 
-
+            binding.tvCourse.text = courseName
+            binding.tvTime.text = timeFormat
+            binding.tvLecturer.text = lecturer
         }
 
         itemView.setOnClickListener {
             clickListener(course)
         }
+    }
+
+    fun clear() {
+        // Clear the view elements if needed
+        binding.tvCourse.text = null
+        binding.tvTime.text = null
+        binding.tvLecturer.text = null
     }
 
     fun getCourse(): Course = course
